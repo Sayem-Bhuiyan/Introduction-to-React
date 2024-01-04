6102fe90407c6f37e7eb209d5f63f38e1f5c804d
Sayem <sayem465508@gmail.com>
2024-01-04 09:03:48 +0600

    introduction to props, props-type, default-props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Button from "./Button.jsx";
-import Button2 from "./Button/Button2.jsx";
+import Student from "./Student.jsx";
 
 function App() {
   return(
     <>
+       <Student name="Sayem Bhuiyan" age={20} isStudent={true}/>
+       <Student name='Fahim Sarker' age={21} isStudent={false}/>
+       <Student name="Ashraful Islam Akash" age={20} isStudent={true}/>
+       <Student name="Shajib Sarkar" age={23} isStudent={true}/>
+       <Student />
        <Button/> 
-       <Button2/>
     </>
   );
 }
--- src/Button/Button2.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Button2.module.css"
-function Button2(){
-    return(
-        <button className={styles.btn}>Subscribe</button>
-    );
-}
-export default Button2;
